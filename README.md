d0569dbedd24da4c1b867860610271cddb710aad
dhruvkar <kar.dhruv@gmail.com>
2015-07-12 02:47:12 -0700

    make files more readable

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# planet labs
+# Planet Labs
 
-find all of the anagrams in a dictionary in which there are at least 4 letters in the word and at least as many anagrams as there are letters.
+Find all of the anagrams in a dictionary in which there are at least 4 letters in the word and at least as many anagrams as there are letters.
 
-testwords.txt is a test file, fairly short in length. 
-words.txt is the full dictionary file, copied from /usr/share/dict/words on my macbook.
+**testwords.txt** is a test file, fairly short in length. 
+
+**words.txt** is the full dictionary file, copied from */usr/share/dict/words* on my macbook.
 
--- anagram.py
+++ anagram.py
@@ -1,20 +1,17 @@
 from __future__ import print_function
 from collections import defaultdict
 
-
 # Open file and add >=4 letter words to a set while stripping newline chars and making lowercase
 with open("words.txt") as f:
 	words = {line.rstrip('\n').lower() for line in f if len(line.rstrip('\n')) >=4}
-	# Created a unique, sorted ('aba' -> 'aab') words set
-	sorted_words = {"".join(sorted(word)) for word in words}
+	
+# Created a unique, sorted ('aba' -> 'aab') words set
+sorted_words = {"".join(sorted(word)) for word in words}
 
 d = defaultdict(list)
 
 # Create a dict with the sorted word as key and all its anagrams as the values
 [d["".join(sorted(word))].append(word) for word in words if "".join(sorted(word)) in sorted_words]
 	
-
-# Cleanly print out anagrams on each line based on problem criteria
-[print(', '.join(v)) for k,v in d.iteritems() if len(k) <= len(v)]
-
-
+# Cleanly print out anagrams on each line if there are at least the same number of anagrams as letters in the word
+[print(', '.join(v)) for k,v in d.iteritems() if len(v) >= len(k)]
